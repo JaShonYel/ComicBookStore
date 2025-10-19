9a635164235da522c9dad4676839760c32c43aec
JaShonYel <jdy9@pct.edu>
2025-10-19 14:22:56 -0400

    add Auth0 integration and update related files

---
+++ Comic-Store-FrontEnd/src/auth0.config.js
@@ -0,0 +1,10 @@
+const authConfig = {
+  domain: process.env.REACT_APP_AUTH0_DOMAIN,
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
+  authorizationParams: {
+    redirect_uri: process.env.REACT_APP_AUTH0_REDIRECT_URI,
+    audience: process.env.REACT_APP_AUTH0_AUDIENCE
+  }
+};
+
+export default authConfig;
